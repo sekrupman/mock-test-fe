c8366121ad7cdf5d03507aa067569aa173b57959
sekrupman <45067224+sekrupman@users.noreply.github.com>
2023-10-11 18:03:03 +0700

    add restrict page

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 
 // IMPORT PAGES
 import App from "../App";
@@ -7,15 +7,18 @@ import Register from "../register/index";
 import Login from "../login";
 import Dashboard from "../dashboard";
 
-
 function Router(){
+  const isAuthenticated = !!localStorage.getItem('userId'); 
     return(
         <Routes>
         <Route path="/" element={<App />} />
         <Route path="register" element={<Register />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
-        <Route path="api/tasks/:userId" element={<Dashboard />} />
+        <Route
+        path="api/tasks/:userId"
+        element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+      />
       </Routes>
     )
 }
